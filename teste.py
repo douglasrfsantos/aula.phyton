89a953dce98c14be6f998f818d30961df295bed7
dg <douglasrafaeldodo@gmail.com>
2024-06-14 15:23:51 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,6 +1,6 @@
 nome = "Douglas"
 idade= 16 
-altura=1.67
+altura=1.75
 maior_de_idade = idade>=18
 print("nome:", nome, "Idade:",idade, "Altura:",altura)
 print("É maior de idade ?", maior_de_idade)
